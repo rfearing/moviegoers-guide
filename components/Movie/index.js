a12a7df0cd47ae907c91758680b99889f020ed57
Ricardo Fearing <ricardo@Ricardos-MacBook-Pro.local>
2019-11-20 14:55:12 -0500

    Move Movie to Seperate Component

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -1,73 +1,70 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { getImageUrl, getExcerpt } from 'BASE/utils';
+import { getImageUrl } from 'BASE/utils';
 import style from './style.scss';
 
-const Movie = ({ movie }) => {
-  const {
-    title,
-    poster_path,
-    id,
-    // backdrop_path,
+const Movie = ({
+  movie: {
+    release_date,
     vote_average,
+    poster_path,
     overview,
-  } = movie;
-
+    revenue,
+    runtime,
+    genres,
+    title,
+  },
+}) => {
   const poster = getImageUrl(poster_path);
-  const excerpt = getExcerpt(overview, 30);
-  const vote = vote_average * 10;
-  let circleColor;
-
-  // Passing or Failing colors
-  if (vote >= 70) {
-    circleColor = 'green';
-  } else if (vote >= 40) {
-    circleColor = 'yellow';
-  } else if (vote > 0) {
-    circleColor = 'red';
-  }
-
-  const circle = (
-    <svg viewBox="0 0 36 36" className={style.chart}>
-      <path
-        className={`${style.circle} ${circleColor}`}
-        strokeDasharray={`${vote}, 100`}
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text
-        className={style.percent}
-        fill="#FFF"
-        x={vote === 100 ? '16%' : '25%'}
-        y="65%"
-      >
-        {vote > 0 ? vote : 'NR'}
-      </text>
-    </svg>
-  );
+  const movieGenres = genres.map((genre) => genre.name).join(', ');
+  const release = new Date(release_date).toDateString();
 
   return (
-    <div className={style.movieItem}>
+    <div className={style.movieBox}>
       <div className={style.poster}>
         <img src={poster} alt={`Movie poster for ${title}`} />
       </div>
       <div className={style.content}>
-        {circle}
-        <h5 className="clearfix">{title}</h5>
-        <p>{excerpt}</p>
-      </div>
-      <div className={style.info}>
-        <hr />
-        <p className="text-right">
-          <Link href={`/movie/${id}`}>
-            <a>Learn more</a>
-          </Link>
-        </p>
-      </div>
+        <h1 className="clearfix">{title}</h1>
+        <p>{overview}</p>
+        {movieGenres && (
+          <p>
+            <b className="green-text">Genres: </b><br />
+            {movieGenres}
+          </p>
+        )}
+        <div className="row">
+          <div className="col-md">
+            <p>
+              <b className="green-text">Release Date: </b><br />
+              {release}
+            </p>
+          </div>
+          <div className="col-md">
+            <p>
+              <b className="green-text">Runtime: </b><br />
+              {runtime} mins
+            </p>
+          </div>
+        </div>
 
+        <div className="row">
+          <div className="col-md">
+            <p>
+              <b className="green-text">Vote Average: </b><br />
+              {vote_average > 0 ? ` ${vote_average} / 10` : 'NA'}
+            </p>
+          </div>
+          <div className="col-md">
+            <p>
+              <b className="green-text">Box Office: </b><br />
+              {revenue > 0 ? `$${revenue.toLocaleString()}` : 'NA'}
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 };
@@ -75,11 +72,14 @@ const Movie = ({ movie }) => {
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
-    poster_path: PropTypes.string,
     id: PropTypes.number,
-    backdrop_path: PropTypes.string,
-    vote_average: PropTypes.number,
     overview: PropTypes.string,
+    vote_average: PropTypes.number,
+    revenue: PropTypes.number,
+    genres: PropTypes.array,
+    poster_path: PropTypes.string,
+    release_date: PropTypes.string,
+    runtime: PropTypes.number,
   }).isRequired,
 };
 
--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -1,89 +1,30 @@
 /* eslint-disable camelcase */
-/* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import { withRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import Header from 'COMPONENTS/Header';
 import Footer from 'COMPONENTS/Footer';
+import Movie from 'COMPONENTS/Movie';
 import { getImageUrl, getHighestVotedImage } from 'BASE/utils';
 import { getMovie, getMovieImages } from 'SERVICES';
 import '../../start.scss';
 import style from './style.scss';
 
-const Movie = ({
-  movie: {
-    release_date,
-    vote_average,
-    poster_path,
-    title,
-    overview,
-    genres,
-    runtime,
-    revenue,
-  },
-  images: {
-    backdrops,
-  },
+const MoviePage = ({
+  movie,
+  images: { backdrops },
+  error,
 }) => {
-  const [error, setError] = useState(false);
   const highest = getHighestVotedImage(backdrops);
-  const poster = getImageUrl(poster_path);
   const imagePath = highest && highest.file_path ? getImageUrl(highest.file_path) : '';
-  const movieGenres = genres.map((genre) => genre.name).join(', ');
-  const release = new Date(release_date).toDateString();
 
   return (
     <>
       <Header />
       <div style={{ backgroundImage: `url(${imagePath})` }} className={`py-5 page-body ${style.background}`}>
         <div className="container">
-          {/* Show error message */}
           {error && (<div className="alert alert-danger" role="alert">{error.message}</div>)}
-          <div className={style.movieBox}>
-            <div className={style.poster}>
-              <img src={poster} alt={`Movie poster for ${title}`} />
-            </div>
-            <div className={style.content}>
-              <h1 className="clearfix">{title}</h1>
-              <p>{overview}</p>
-              {movieGenres && (
-                <p>
-                  <b className="green-text">Genres: </b><br />
-                  {movieGenres}
-                </p>
-              )}
-              <div className="row">
-                <div className="col-md">
-                  <p>
-                    <b className="green-text">Release Date: </b><br />
-                    {release}
-                  </p>
-                </div>
-                <div className="col-md">
-                  <p>
-                    <b className="green-text">Runtime: </b><br />
-                    {runtime} mins
-                  </p>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md">
-                  <p>
-                    <b className="green-text">Vote Average: </b><br />
-                    {vote_average > 0 ? ` ${vote_average} / 10` : 'NA'}
-                  </p>
-                </div>
-                <div className="col-md">
-                  <p>
-                    <b className="green-text">Box Office: </b><br />
-                    {revenue > 0 ? `$${revenue.toLocaleString()}` : 'NA'}
-                  </p>
-                </div>
-              </div>
-
-            </div>
-          </div>
+          <Movie movie={movie} />
         </div>
       </div>
       <Footer />
@@ -91,33 +32,36 @@ const Movie = ({
   );
 };
 
-Movie.getInitialProps = async ({ query }) => {
+MoviePage.getInitialProps = async ({ query }) => {
   const { id } = query;
-  const [movie, images] = await Promise.all([
-    getMovie(id),
-    getMovieImages(id),
-  ]);
-
-  return { movie, images };
+  try {
+    const [movie, images] = await Promise.all([
+      getMovie(id),
+      getMovieImages(id),
+    ]);
+    return { movie, images };
+  } catch (error) {
+    return error;
+  }
 };
 
-Movie.propTypes = {
+MoviePage.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
-    id: PropTypes.number,
-    overview: PropTypes.string,
-    vote_average: PropTypes.number,
     genres: PropTypes.array,
   }),
   images: PropTypes.shape({
     backdrops: PropTypes.array,
-    images: PropTypes.array,
+  }),
+  error: PropTypes.shape({
+    message: PropTypes.string,
   }),
 };
 
-Movie.defaultProps = {
+MoviePage.defaultProps = {
   movie: {},
   images: [],
+  error: null,
 };
 
-export default withRouter(Movie);
+export default withRouter(MoviePage);
